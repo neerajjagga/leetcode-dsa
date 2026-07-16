2cfa1e2df90187eb85065b6d146432ad94b61159
Neeraj Jagga <neerajjagga26@gmail.com>
2026-07-16 10:59:59 +0530

    Time: 2 ms (88.31%), Space: 128.2 MB (78.09%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,54 +1,20 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        
-        // Brute force
+        vector<int> result(nums.size());
+        int evenIndex = 0, oddIndex = 1;
 
-        // int n = nums.size();
-        
-        // vector<int>pos;
-        // vector<int>neg;
-        // vector<int>ans;
-
-        // // dividing postive and negative numbers
-        // for(int i=0; i<n; i++) {
-        //     if(nums[i] > 0) {
-        //     pos.push_back(nums[i]);
-        //     }
-        //     else {
-        //     neg.push_back(nums[i]);
-        //     }
-        // }
-    
-        // // pushing elements into ans array 
-        // // in order 
-        // for(int i=0; i<n/2; i++) {
-        //     ans.push_back(pos[i]);
-        //     ans.push_back(neg[i]);
-        // }
-
-        // return ans;
-
-        
-
-        // optimal approach
-
-        int n = nums.size();
-        int even = 0, odd = 1;
-        vector<int> ans(n);
-
-        for(int i=0; i<n; i++) {
-            int num = nums[i];
-
-            if(num >= 0) {
-            ans[even] = nums[i];
-            even += 2;
-            } else {
-            ans[odd] = nums[i];
-            odd += 2;
+        for(int num: nums) {
+            if(num > 0) {
+                result[evenIndex] = num;
+                evenIndex += 2;
+            }
+            else {
+                result[oddIndex] = num;
+                oddIndex += 2;
             }
         }
 
-        return ans;
+        return result;
     }
 };
