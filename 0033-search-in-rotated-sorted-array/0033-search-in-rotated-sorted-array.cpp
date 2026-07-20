058aa37bc1e9d339071e60a7624611a43e1f6977
Neeraj Jagga <neerajjagga26@gmail.com>
2026-07-20 18:04:38 +0530

    Time: 0 ms (100%), Space: 15.3 MB (35.1%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,52 +1,32 @@
 class Solution {
 public:
-    int findPivotIndex(vector<int>& nums) {
-        int n = nums.size();
-
-        // array is already sorted
-        if (nums[0] <= nums[n - 1])
-            return n - 1;
-
-        int low = 0, end = n-1;
-
-        while(low <= end) {
-            if(low == end) return low;
-
-            int mid = low + (end - low) / 2;
-
-            if(mid-1 >= 0 && nums[mid] < nums[mid - 1]) return mid - 1;
-            else if(mid+1 < n && nums[mid] > nums[mid+1]) return mid;
-            else if(nums[low] > nums[mid]) end = mid - 1;
-            else low = mid + 1;
-        }
-        return -1;
-    }
-
-    int binarySearch(vector<int>& nums, int low, int end, int target) {
-        while(low <= end) {
-            int mid = low + (end - low) / 2;
-            
-            if(nums[mid] == target) return mid;
-            else if(nums[mid] < target) low = mid + 1;
-            else end = mid - 1; 
-        }
-
-        return -1;
-    }
-
     int search(vector<int>& nums, int target) {
-        int pivotIndex = findPivotIndex(nums);
-        
-        int ans  = -1;
+        int n = nums.size();
+        int low = 0, high = n - 1;
 
-        // search in A
-        if(target >= nums[0] && target <= nums[pivotIndex]) {
-            ans = binarySearch(nums, 0, pivotIndex, target);
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] == target) return mid;
+            
+            // find sorted half array
+            // know if first half is sorted or not
+            // it means first half is sorted
+            if(nums[low] <= nums[mid]) {
+                // now try to find the target in sorted half
+                if(nums[low] <= target && target <= nums[mid])
+                    high = mid - 1;
+
+                // if not then go to unsorted half
+                else low = mid + 1;
+            }
+            else {
+                if(nums[mid] <= target && target <= nums[high])
+                    low = mid + 1;
+                else high = mid - 1;
+            }
         }
-        else { 
-            // search in B
-            ans = binarySearch(nums, pivotIndex+1, nums.size() - 1, target);
-        }
-        return ans;
+
+        return -1;
     }
 };
