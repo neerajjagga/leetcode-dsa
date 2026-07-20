3b276ae90d48e934a95cf259bd172547f2f25704
Neeraj Jagga <neerajjagga26@gmail.com>
2026-07-20 17:26:05 +0530

    Time: 0 ms (100%), Space: 15 MB (96.57%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,46 +1,42 @@
 class Solution {
 public:
-
-    int findPivotIndex(vector<int> &nums) {
+    int findPivotIndex(vector<int>& nums) {
         int n = nums.size();
-        int s = 0;
-        int e = n-1;
 
-        while(s <= e) {
-            int mid = s + (e-s)/2;
-            // if array contains only 1 element 
-            if(s == e) return s;
-            // add condition (mid-1 >= 0) so that our index should be valid index
-            else if(mid-1 >= 0 && nums[mid] < nums[mid-1]) return mid-1;
-            // add condition (mid+1 < n ) so that our index should be valid index
+        // array is already sorted
+        if (nums[0] <= nums[n - 1])
+            return n - 1;
+
+        int low = 0, end = n-1;
+
+        while(low <= end) {
+            if(low == end) return low;
+
+            int mid = low + (end - low) / 2;
+
+            if(mid-1 >= 0 && nums[mid] < nums[mid - 1]) return mid - 1;
             else if(mid+1 < n && nums[mid] > nums[mid+1]) return mid;
-            else if(nums[s] > nums[mid]) e = mid-1;
-            else s = mid+1;
+            else if(nums[low] > nums[mid]) end = mid - 1;
+            else low = mid + 1;
         }
         return -1;
     }
 
-    int binarySearch(vector<int> &nums,int s, int e, int target) {
-        int n = nums.size();
-
-        while(s <= e) {
-            int mid = s + (e-s)/2;
-            if(nums[mid] == target) {
-                return mid;
-            }
-            else if(nums[mid] < target) {
-                s = mid + 1;
-            }
-            else {
-                e = mid - 1;
-            }
+    int binarySearch(vector<int>& nums, int low, int end, int target) {
+        while(low <= end) {
+            int mid = low + (end - low) / 2;
+            
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) low = mid + 1;
+            else end = mid - 1; 
         }
+
         return -1;
     }
 
     int search(vector<int>& nums, int target) {
         int pivotIndex = findPivotIndex(nums);
-        int n = nums.size();
+        
         int ans  = -1;
 
         // search in A
@@ -49,7 +45,7 @@ public:
         }
         else { 
             // search in B
-            ans = binarySearch(nums, pivotIndex+1, n-1, target);
+            ans = binarySearch(nums, pivotIndex+1, nums.size() - 1, target);
         }
         return ans;
     }
