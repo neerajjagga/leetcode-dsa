41007f033981a5a4d5197405965b666612c4e51b
Neeraj Jagga <neerajjagga26@gmail.com>
2026-08-23 13:54:33 +0530

    Time: 35 ms (53.24%), Space: 45.3 MB (28.67%) - LeetHub

---
+++ 3628-maximum-number-of-subsequences-after-one-inserting/3628-maximum-number-of-subsequences-after-one-inserting.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    long long numOfSubsequences(string s) {
+        int n = s.length();
+
+        vector<long long> prefix(n+1, 0);
+        vector<long long> suffix(n+1, 0);
+
+        for(int i=0; i<n; i++) {
+            if(s[i] == 'L') prefix[i+1] = 1;
+            prefix[i+1] += prefix[i];
+        }
+
+        for(int i=n-1; i>=0; --i) {
+            if(s[i] == 'T') suffix[i] = 1;
+            suffix[i] += suffix[i+1];
+        }
+
+        long long p1 = 0;
+        long long p2 = 0;
+        long long p3 = 0;
+        long long maxi = 0;
+
+        /*
+            For every existing C:
+
+            Calculate its contribution if L is inserted
+            Calculate its contribution if T is inserted
+            Calculate its existing contribution
+        */
+        for(int i=0; i<n; i++) {
+            if(s[i] == 'C') {
+                p1 += (prefix[i+1] + 1) * suffix[i];
+                p2 += (suffix[i] + 1) * prefix[i+1];
+                p3 += prefix[i+1] * suffix[i];
+            }
+            maxi = max(maxi, prefix[i+1] * suffix[i]);
+        }
+
+        p3 += maxi;
+
+        return max({ p1, p2, p3 });
+    }
+};
