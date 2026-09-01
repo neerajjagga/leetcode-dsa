7fac4f5b8401c17a1e8c38a8f279d919ba7a4119
Neeraj Jagga <neerajjagga26@gmail.com>
2026-09-01 11:25:28 +0530

    Time: 3 ms (37.41%), Space: 40.3 MB (44.56%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,23 +1,49 @@
 class Solution {
 public:
+
+    // Space = O(N) excluding Auxiliary space
+    // vector<int> productExceptSelf(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<int> prefixProduct(n+1, 1);
+    //     vector<int> ans;
+
+    //     for(int i=0; i<n; i++) {
+    //         prefixProduct[i+1] = prefixProduct[i] * nums[i]; 
+    //     }
+
+    //     vector<int> suffixProduct(n+1, 1);
+
+    //     for(int i=n-1; i>=0; i--) {
+    //         suffixProduct[i] = suffixProduct[i+1] * nums[i];
+    //     }
+
+    //     for(int i=0; i<n; i++) {
+    //         int product = prefixProduct[i] * suffixProduct[i+1];
+    //         ans.push_back(product);
+    //     }
+
+    //     return ans;
+    // }
+
+
+
+
+    // Space = O(1) excluding Auxiliary space
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> prefixProduct(n+1, 1);
-        vector<int> ans;
+        vector<int> ans(n);
 
+        int prefix = 1;
+        // pre store left product in ans
         for(int i=0; i<n; i++) {
-            prefixProduct[i+1] = prefixProduct[i] * nums[i]; 
+            ans[i] = prefix;
+            prefix *= nums[i];
         }
 
-        vector<int> suffixProduct(n+1, 1);
-
+        int suffix = 1;
         for(int i=n-1; i>=0; i--) {
-            suffixProduct[i] = suffixProduct[i+1] * nums[i];
-        }
-
-        for(int i=0; i<n; i++) {
-            int product = prefixProduct[i] * suffixProduct[i+1];
-            ans.push_back(product);
+            ans[i] *= suffix;
+            suffix *= nums[i];
         }
 
         return ans;
