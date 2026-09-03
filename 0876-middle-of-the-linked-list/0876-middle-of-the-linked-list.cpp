40f8442821204f2abaee442de6c07660643cae6b
Neeraj Jagga <neerajjagga26@gmail.com>
2026-09-03 10:47:42 +0530

    Time: 0 ms (100%), Space: 10 MB (64.54%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -10,23 +10,35 @@
  */
 class Solution {
 public:
+    // two pass solution
+    // ListNode* middleNode(ListNode* head) {
+    //     int length = 0;
+    //     ListNode* temp = head;
+    //     while(temp != nullptr) {
+    //         length++;
+    //         temp = temp->next;
+    //     }
+
+    //     int mid = length / 2;
+
+    //     temp = head;
+    //     for(int i=0; i<mid; i++) {
+    //         temp = temp->next; 
+    //     }
+
+    //     return temp;
+    // }
+    
+
+    // one pass solution - slow and fast pointers
     ListNode* middleNode(ListNode* head) {
-        int length = 0;
-        ListNode* temp = head;
-        while(temp != nullptr) {
-            length++;
-            temp = temp->next;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        if(length == 1) return head;
-
-        int mid = length / 2;
-
-        temp = head;
-        for(int i=0; i<mid; i++) {
-            temp = temp->next; 
-        }
-
-        return temp;
+        return slow;
     }
 };
