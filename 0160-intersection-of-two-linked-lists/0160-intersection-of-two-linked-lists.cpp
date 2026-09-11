d8a4084cf0c53c497235d0f984d166aecf77f971
Neeraj Jagga <neerajjagga26@gmail.com>
2026-09-11 22:17:18 +0530

    Time: 56 ms (58.33%), Space: 23.9 MB (85.62%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,24 +8,45 @@
  */
 class Solution {
 public:
+    // Space comp = O(N1)
+    // ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    //     unordered_map<ListNode*, int> mpp;
+
+    //     // traverse list 1
+    //     ListNode* curr = headA;
+    //     while(curr) {
+    //         mpp[curr] = 1;
+    //         curr = curr->next;
+    //     }
+
+    //     // traverse 2
+    //     curr = headB;
+    //     while(curr) {
+    //         if(mpp.count(curr))
+    //             return curr;
+    //         curr = curr->next;
+    //     }
+
+    //     return nullptr;
+    // }
+
+
+    // Optimized, Time comp - O(N1 + N2), Space - O(1)
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_map<ListNode*, int> mpp;
+        if(headA == nullptr || headB == nullptr) return nullptr;
 
-        // traverse list 1
-        ListNode* curr = headA;
-        while(curr) {
-            mpp[curr] = 1;
-            curr = curr->next;
+        ListNode* temp1 = headA;
+        ListNode* temp2 = headB;
+        while(temp1 != temp2) {
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+
+            if(temp1 == temp2) return temp1;
+
+            if(temp1 == nullptr) temp1 = headB;
+            if(temp2 == nullptr) temp2 = headA;
         }
 
-        // traverse 2
-        curr = headB;
-        while(curr) {
-            if(mpp.count(curr))
-                return curr;
-            curr = curr->next;
-        }
-
-        return nullptr;
+        return temp1;
     }
 };
