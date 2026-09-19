340b2d40820406438607f23cde8d96cf5daa5d53
Neeraj Jagga <neerajjagga26@gmail.com>
2026-09-19 16:44:23 +0530

    Time: 0 ms (100%), Space: 8.1 MB (23.52%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
+    // bool rotateString(string s, string goal) {
+    //     if(s.length() != goal.length()) return false;
+    //     if(s == goal) return true;
+
+    //     for(int i=1; i<s.length(); i++) {
+    //        string rotated = s.substr(i) + s.substr(0, i);
+    //        if(rotated == goal) return true;
+    //     }
+    //     return false;
+    // }
+
+
+    // another approach - if you contcatenate a string with itself, it will contain all rotations as a substring in it
     bool rotateString(string s, string goal) {
         if(s.length() != goal.length()) return false;
         if(s == goal) return true;
 
-        for(int i=1; i<s.length(); i++) {
-           string rotated = s.substr(i) + s.substr(0, i);
-           if(rotated == goal) return true;
-        }
-        return false;
+        return (s+s).find(goal) != string::npos;
     }
 };
